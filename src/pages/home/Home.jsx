84de9f06b92b1fdcd995cc87b5166597999d503e
arindom10 <karindom7@gmail.com>
2024-07-25 19:18:32 +0600

    dark mode added in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
+import myContext from "../../context/data/myContext";
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  const context = useContext(myContext);
+  const { toggleMode, mode } = context;
+  return (
+    <div
+      style={{
+        color: mode === "dark" ? "white" : "",
+      }}
+    >
+      <Layout>
+        <p>Home</p>
+      </Layout>
+    </div>
+  );
 };
 
 export default Home;
